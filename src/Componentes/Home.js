f6ba86dacfbd4bf52b5353e1b3a8747c7c34c29d
mariabenning <mariabenning15@gmail.com>
2021-07-21 20:44:37 -0300

    adicionado as imagens, nomes e valores dos produtos no componente home

--- src/Componentes/Home.js
+++ src/Componentes/Home.js
@@ -1,12 +1,51 @@
 import React from 'react'
 
 
+
+
+
+
+
 export default class Home extends React.Component {
 
-    render () {
+    Produtos
+
+    render() {
         return (
             <div>
-                <p>Home</p>
+                <p>Produtos</p>
+                
+                <div className="card-produto">
+                
+                    <p>Nave espacial </p>
+                    <img src="https://st.depositphotos.com/1320280/4156/i/950/depositphotos_41564581-stock-photo-unidentified-flying-object.jpg" alt=" imagem  espacial" ></img>
+                    
+                    <p>Valor: 30.000,00</p>
+                    <button>Adicionar ao carinho</button>
+                    
+            
+            
+                    <img src="https://st2.depositphotos.com/2197700/9185/i/950/depositphotos_91851398-stock-photo-space-shuttle-flying-over-the.jpg" alt=" imagem  espacial" ></img>
+                    <p>Foguete</p>
+                    <p>valor: 50.000,00</p>
+                    <button>Adicionar ao carinho</button>
+
+
+                    <p> Foguete Branco</p>
+                    <img src="https://st3.depositphotos.com/1006542/13733/i/1600/depositphotos_137335916-stock-photo-launch-of-space-missile.jpg" alt=" imagem  espacial" ></img>
+                    <p>valor: 55.000,00</p>
+                    <button>Adicionar ao carinho</button>
+        
+                    </div>
+
+                
+
+
+
+
+
+
+
             </div>
         )
     }
