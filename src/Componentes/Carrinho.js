bc5c71af25ad1d57e0886f40e87bebf53c6df924
Samyr Chiade Hissa Filho <samyr@schissa.com.br>
2021-07-21 15:07:09 -0300

    labe-commerce-samyr

---
+++ src/Componentes/Carrinho.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+export default class Carrinho extends React.Component {
+
+    render () {
+        return (
+            <div>
+                <p>Carrinho</p>
+            </div>
+        )
+    }
+}
---
+++ src/Componentes/Filtro.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+export default class Filtro extends React.Component {
+
+    render () {
+        return (
+            <div>
+                <p>Filtro</p>
+            </div>
+        )
+    }
+}
