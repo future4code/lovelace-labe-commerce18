2b883d2ea1f797582d8005162742f3d3d7114d72
mariabenning <mariabenning15@gmail.com>
2021-07-22 14:55:12 -0300

    criado array de objeto e ajustado tamaho das imagens

--- src/Componentes/Home.js
+++ src/Componentes/Home.js
@@ -1,40 +1,96 @@
 import React from 'react'
+import styled from 'styled-components'
+
+
+const StiloDasImagens = styled.img
+`
+width: 200px;
+border: 1px solid black;
+display flex;
+justify-content: center;
+align-items: center;
+
+
+`
+
+const DescricaoProdutos = styled.p
+`
+display flex;
+text-align: center
+`
+
+
+const ValorDoProduto = styled.p
+`
+display flex;
+align-items: center;
+`
 
 
 
 
+const BotaoAdicionar = styled.button 
+`
+display flex; 
+
+`
+
+
+
+const  listaDeProdutos = [
+{
+id: 1,
+descricao: "SpaçoNave",
+valor: 30000.00,
+urlProduto:"https://st.depositphotos.com/1320280/4156/i/950/depositphotos_41564581-stock-photo-unidentified-flying-object.jpg"
+
+},
+{
+id: 2,
+descricao: "Foguete",
+valor: 50000.00,
+urlProduto:"https://st2.depositphotos.com/2197700/9185/i/950/depositphotos_91851398-stock-photo-space-shuttle-flying-over-the.jpg"
+},
+
+{
+id: 3,
+descricao: "Foguete Branco",
+valor: 55000.00,
+urlProduto:"https://st3.depositphotos.com/1006542/13733/i/1600/depositphotos_137335916-stock-photo-launch-of-space-missile.jpg"
+}
+]
 
 
 
 export default class Home extends React.Component {
 
-    Produtos
+    
 
     render() {
         return (
             <div>
-                <p>Produtos</p>
+                <p>Home</p>
                 
                 <div className="card-produto">
-                
-                    <p>Nave espacial </p>
-                    <img src="https://st.depositphotos.com/1320280/4156/i/950/depositphotos_41564581-stock-photo-unidentified-flying-object.jpg" alt=" imagem  espacial" ></img>
+                    <StiloDasImagens src={listaDeProdutos[0].urlProduto} alt=" EspaçoNave" ></StiloDasImagens>
+                    <DescricaoProdutos>{listaDeProdutos[0].descricao} </DescricaoProdutos>
+                    <ValorDoProduto>{listaDeProdutos[0].valor}</ValorDoProduto>
+                    <BotaoAdicionar>Adicionar ao carrinho</BotaoAdicionar>
                     
-                    <p>Valor: 30.000,00</p>
-                    <button>Adicionar ao carinho</button>
-                    
-            
-            
-                    <img src="https://st2.depositphotos.com/2197700/9185/i/950/depositphotos_91851398-stock-photo-space-shuttle-flying-over-the.jpg" alt=" imagem  espacial" ></img>
-                    <p>Foguete</p>
-                    <p>valor: 50.000,00</p>
-                    <button>Adicionar ao carinho</button>
+              
+                     <StiloDasImagens src={listaDeProdutos[1].urlProduto} alt="foguete" ></StiloDasImagens>
+                    <DescricaoProdutos>{listaDeProdutos[1].descricao} </DescricaoProdutos>
+                    <ValorDoProduto>{listaDeProdutos[1].valor}</ValorDoProduto>
+                    <BotaoAdicionar>Adicionar ao carrinho</BotaoAdicionar>
 
 
-                    <p> Foguete Branco</p>
-                    <img src="https://st3.depositphotos.com/1006542/13733/i/1600/depositphotos_137335916-stock-photo-launch-of-space-missile.jpg" alt=" imagem  espacial" ></img>
-                    <p>valor: 55.000,00</p>
-                    <button>Adicionar ao carinho</button>
+                    
+                    <StiloDasImagens src={listaDeProdutos[2].urlProduto} alt="foguete branco" ></StiloDasImagens>                   
+                    <DescricaoProdutos>{listaDeProdutos[2].descricao} </DescricaoProdutos>
+                    <ValorDoProduto>{listaDeProdutos[2].valor}</ValorDoProduto>
+                    <BotaoAdicionar>Adicionar ao carrinho</BotaoAdicionar>
+                    
+                    
         
                     </div>
 
