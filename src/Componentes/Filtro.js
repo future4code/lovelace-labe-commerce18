7393175d030c56138341a7f1f7af148faf7bf23f
Muri-lo <muriloss2019@hotmail.com>
2021-07-21 22:26:05 -0300

    Tentei estabelecer uma lógica do layout

--- src/Componentes/Filtro.js
+++ src/Componentes/Filtro.js
@@ -1,12 +1,45 @@
 import React from 'react'
 
+class filtro extends React.Component {
+    state = [
+        {
+            id: 1,
+            name: "Foguete da Missão Apollo 11",
+            value: 10000.0,
+            imageUrl: "https://picsum.photos/200/200",
+        },
+        {
+            id: 1,
+            name: "Foguete da Missão Apollo 10",
+            value: 10000.0,
+            imageUrl: "https://picsum.photos/200/200",
+        },
+        {
+            id: 1,
+            name: "Foguete da Missão Apollo 9",
+            value: 10000.0,
+            imageUrl: "https://picsum.photos/200/200",
+        }
+    ]
+}
+
 
 export default class Filtro extends React.Component {
 
     render () {
         return (
             <div>
-                <p>Filtro</p>
+                <h3>Filtros</h3>
+                    <label>
+                    "Valor Mínimo:"
+                    </label>
+                    <label>
+                    "Valor Máximo:"
+                    </label>
+                    <label>
+                    "Busca por nome:"
+                     </label>
+
             </div>
         )
     }
